2e07ce27c74e192d5aedb2b34e4258b296dab4d1
Liquor4k993 <kiratuber01@gmail.com>
2025-11-16 19:16:46 +0300

    Задание 1

--- src/main/java/me/liquor4k/Main.java
+++ src/main/java/me/liquor4k/Main.java
@@ -1,2 +1,12 @@
 package me.liquor4k;
 
+// Задача 1
+public class Main {
+    public static void main(String[] args) {
+        String firstName = "Иван";
+        String middleName = "Иванович";
+        String lastName = "Иванов";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+    }
+}
